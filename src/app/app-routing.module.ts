5fb8eec3f1d5e74e6b869995eaab5727cb7243b4
Innocent Suta <sutainnocent@gmail.com>
2020-08-22 18:52:06 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './modules/pages/home/home.component';
+import { AboutComponent } from './modules/pages/about/about.component';
+import { PodcastComponent } from './modules/pages/podcast/podcast.component';
+import { VideosComponent } from './modules/pages/videos/videos.component';
+import { ContactComponent } from './modules/pages/contact/contact.component';
+import { BlogComponent } from './modules/pages/blog/blog.component';
+import { EventsComponent } from './modules/pages/events/events.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'podcast', component: PodcastComponent },
+  { path: 'videos', component: VideosComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'blog', component: BlogComponent},
+  { path: 'events', component: EventsComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/pages/home/home.component';
+import { AboutComponent } from './modules/pages/about/about.component';
+import { PodcastComponent } from './modules/pages/podcast/podcast.component';
+import { VideosComponent } from './modules/pages/videos/videos.component';
+import { EventsComponent } from './modules/pages/events/events.component';
+import { BlogComponent } from './modules/pages/blog/blog.component';
+import { ContactComponent } from './modules/pages/contact/contact.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PodcastComponent,
+    VideosComponent,
+    EventsComponent,
+    BlogComponent,
+    ContactComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
